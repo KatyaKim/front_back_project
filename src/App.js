14100317a8f6b113da17683ebf599325ed489b49
Kim <kim@MacBook-AirEkaterina.local>
2022-06-14 17:17:28 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SearchAppBar from "./Components/Navbar/Navbar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <SearchAppBar />
+    </div>
+  );
 };
 
 export default App;
